04a24aaa329ae17530154c41a0f238c944b05dc4
jamieboy1337 <jamieboy1337@gmail.com>
2021-03-24 18:06:16 -0700

    added draw pile tests

---
+++ test/DrawPileTests.js
@@ -0,0 +1,38 @@
+const expect = require("chai").expect;
+const DrawPile = require("../game/cardpile/DrawPile");
+const { GingloidCard, CardColor, CardValue } = require("../game/GingloidCard");
+
+describe("Draw pile", function() {
+  describe("Modifying draw piles", function() {
+    it("Can store and retrieve a card", function() {
+      let pile = new DrawPile();
+      pile.addCard(new GingloidCard(CardColor.RED, CardValue.ONE, 1));
+      let card = pile.drawCard();
+      expect(card.color).to.equal(CardColor.RED);
+      expect(card.value).to.equal(CardValue.ONE);
+      expect(card.id).to.equal(1);
+    });
+
+    it("Can store and retrieve multiple cards", function() {
+      let pile = new DrawPile();
+      pile.addCard(new GingloidCard(CardColor.RED, CardValue.ONE, 1));
+      pile.addCard(new GingloidCard(CardColor.BLUE, CardValue.TWO, 2));
+      pile.addCard(new GingloidCard(CardColor.YELLOW, CardValue.THREE, 3));
+      
+      let cardDraw = pile.drawCard();
+      expect(cardDraw.color).to.equal(CardColor.YELLOW);
+      expect(cardDraw.value).to.equal(CardValue.THREE);
+      expect(cardDraw.id).to.equal(3);
+
+      cardDraw = pile.drawCard();
+      expect(cardDraw.color).to.equal(CardColor.BLUE);
+      expect(cardDraw.value).to.equal(CardValue.TWO);
+      expect(cardDraw.id).to.equal(2);
+
+      cardDraw = pile.drawCard();
+      expect(cardDraw.color).to.equal(CardColor.RED);
+      expect(cardDraw.value).to.equal(CardValue.ONE);
+      expect(cardDraw.id).to.equal(1);
+    });
+  });
+});
