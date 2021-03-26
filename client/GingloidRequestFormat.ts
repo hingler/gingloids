04b6aa51bf75aadca8ef56405d4f156d85ee197c
jamieboy1337 <jamieboy1337@gmail.com>
2021-03-26 13:36:31 -0700

    some interface hints

---
+++ client/GingloidRequestFormat.ts
@@ -0,0 +1,14 @@
+interface GingloidJoinGamePacket {
+  game: string; // token associated with the game.
+  name: string; // name of the new player.
+};
+
+interface GingloidPlayCardPacket {
+  play: string; // the action performed by the client (typically: draw/play)
+  card: number; // the ID of the card which was played by the client (if play).
+  opts: {
+    color: string;  // if a pick card is played, this specifies its color.
+  }
+}
+
+export { GingloidJoinGamePacket, GingloidPlayCardPacket };
