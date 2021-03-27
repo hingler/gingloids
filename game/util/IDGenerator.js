5a99483196536446677d447c30fa9c78ac21ee8c
jamieboy1337 <jamieboy1337@gmail.com>
2021-03-27 03:52:19 -0700

    oops lol

--- game/util/IDGenerator.js
+++ game/util/IDGenerator.js
@@ -1,11 +1,13 @@
+const getRandomValues = require("get-random-values");
+
 /**
  * 
  * @param {Number} len - number of hex chars to generate.
  * @returns {String} randomly generated string of {len} hex chars.
  */
 function generateId(len) {
-  return crypto.getRandomValues(new Int8Array(len)).reduce((prev, acc) => {
-    acc = acc.concat(acc, (prev & 15).toString(16));
+  return getRandomValues(new Uint8Array(len)).reduce((acc, prev) => {
+    return acc.concat((prev & 15).toString(16));
   }, "");
 }
 
