81d697eb732cdef4bb295f70fa49e11cef9495d4
jamieboy1337 <jamieboy1337@gmail.com>
2021-03-29 02:22:58 -0700

    read port from env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const ws = require("ws");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // communicate via websocket
 // send game state
